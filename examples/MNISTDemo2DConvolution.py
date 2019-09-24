635e68c0ea932b8bba2a8bb32ab88a8892033418
Rupsa Saha <rupsajina@gmail.com>
2019-09-24 16:09:15 +0200

    Update MNISTDemo2DConvolution.py

--- examples/MNISTDemo2DConvolution.py
+++ examples/MNISTDemo2DConvolution.py
@@ -6,14 +6,19 @@ import numpy as np
 from time import time
 
 from keras.datasets import mnist
+NUM_CLAUSES=10
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+CLASSES=list(set(Y_train)) #list of classes
 
 X_train = np.where(X_train >= 75, 1, 0) 
 X_test = np.where(X_test >= 75, 1, 0) 
 
 #tm = MultiClassConvolutionalTsetlinMachine2D(8000, 200, 10.0, (10, 10))
-tm = MultiClassConvolutionalTsetlinMachine2D(10, 27, 15.0, (10, 10))
+tm = MultiClassConvolutionalTsetlinMachine2D(NUM_CLAUSES, 27, 15.0, (10, 10))
+
+NUM_FEATURES=len(X_train[0])
 
 print("\nAccuracy over 1 epochs:\n")
 for i in range(1):
@@ -24,6 +29,11 @@ for i in range(1):
 	result = 100*(tm.predict(X_test) == Y_test).mean()
 	
 	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+
+	
+print('Num Clauses:', NUM_CLAUSES)
+print('Num Classes: ', len(CLASSES),' : ', CLASSES)
+print('Num Features: ', NUM_FEATURES)
 	
 for cur_clause in range(NUM_CLAUSES):
 	for cur_cls in CLASSES:
