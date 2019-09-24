79ceb54f416406eb59821cc5989c7723ddbe9239
Rupsa Saha <rupsajina@gmail.com>
2019-09-24 15:52:05 +0200

    Update MNISTDemo2DConvolution.py

--- examples/MNISTDemo2DConvolution.py
+++ examples/MNISTDemo2DConvolution.py
@@ -13,7 +13,7 @@ X_train = np.where(X_train >= 75, 1, 0)
 X_test = np.where(X_test >= 75, 1, 0) 
 
 #tm = MultiClassConvolutionalTsetlinMachine2D(8000, 200, 10.0, (10, 10))
-tm = MultiClassConvolutionalTsetlinMachine2D(80, 27, 15.0, (10, 10))
+tm = MultiClassConvolutionalTsetlinMachine2D(10, 27, 15.0, (10, 10))
 
 print("\nAccuracy over 1 epochs:\n")
 for i in range(1):
@@ -24,3 +24,25 @@ for i in range(1):
 	result = 100*(tm.predict(X_test) == Y_test).mean()
 	
 	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+	
+for cur_clause in range(NUM_CLAUSES):
+	for cur_cls in CLASSES:
+		this_clause=''
+		for f in range(NUM_FEATURES*2):
+			action = tm.ta_action(int(cur_cls), cur_clause, f)
+			if action==1:
+				if this_clause!='':
+					this_clause+='AND '
+				if f<NUM_FEATURES:
+					this_clause+='F'+str(f)+' '
+				else:
+					this_clause+='-|F'+str(f-NUM_FEATURES)+' '
+
+		print('CLASS :',cur_cls,' - CLAUSE ',cur_clause, ' : ', this_clause)
+	print('\n\n')
+
+
+for cur_cls in CLASSES:
+	print('Class ',cur_cls)
+	for cur_clause in range(NUM_CLAUSES):
+		print (cur_clause,':',tm.get_typeII_clauses(int(cur_cls), cur_clause))
