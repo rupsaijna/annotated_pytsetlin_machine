7fb41ec9ee1faf5ac00a5890b5931a9fe441cc3a
Rupsa Saha <rupsajina@gmail.com>
2019-10-02 10:32:53 +0200

    Update conv_artificialdata_demo.py

--- examples/conv_artificialdata_demo.py
+++ examples/conv_artificialdata_demo.py
@@ -29,6 +29,7 @@ THRESHOLD= 15
 SP=2.0
 RUN_EPOCHS=1
 tm = MultiClassConvolutionalTsetlinMachine2D(NUM_CLAUSES, THRESHOLD, SP, (2, 2))
+NUM_FEATURES=int(2*2 + (3 - 2) + (3 - 2))
 
 print("\nAccuracy over 1 epochs:\n")
 for i in range(RUN_EPOCHS):
@@ -38,3 +39,24 @@ for i in range(RUN_EPOCHS):
 	result = 100*(tm.predict(X_test) == Y_test).mean()
 	
 	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+	
+	
+print('Num Clauses:', NUM_CLAUSES)
+print('Num Classes: ', len(CLASSES),' : ', CLASSES)
+print('Num Features: ', NUM_FEATURES)
+	
+for cur_clause in range(NUM_CLAUSES):
+	for cur_cls in CLASSES:
+		this_clause=''
+		for f in range(NUM_FEATURES*2):
+			action = tm.ta_action(int(cur_cls), cur_clause, f)
+			if action==1:
+				if this_clause!='':
+					this_clause+='AND '
+				if f<NUM_FEATURES:
+					this_clause+='F'+str(f)+' '
+				else:
+					this_clause+='-|F'+str(f-NUM_FEATURES)+' '
+
+		print('CLASS :',cur_cls,' - CLAUSE ',cur_clause, ' : ', this_clause)
+	print('\n\n')
