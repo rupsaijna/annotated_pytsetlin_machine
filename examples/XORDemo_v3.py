4e2b06553e171e3c8aef79aba96fef5b28ef31e1
Rupsa Saha <rupsajina@gmail.com>
2019-10-03 15:02:28 +0200

    Update XORDemo_v3.py

--- examples/XORDemo_v3.py
+++ examples/XORDemo_v3.py
@@ -77,26 +77,10 @@ for ep in range(EPOCHS):
 	#print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
 	
 	all_clauses=tm.get_all_clauses(CLASSES,NUM_CLAUSES, NUM_FEATURES)
+	
+	print('all_clauses', all_clauses)
 
-	'''all_clauses=[[] for i in range (NUM_CLAUSES)]
-
-	for cur_clause in range(NUM_CLAUSES):
-		for cur_cls in CLASSES:
-			this_clause=''
-			for f in range(NUM_FEATURES*2):
-				action = tm.ta_action(int(cur_cls), cur_clause, f)
-				if action==1:
-					if this_clause!='':
-						this_clause+='AND '
-					if f<NUM_FEATURES:
-						this_clause+='F'+str(f)+' '
-					else:
-						this_clause+='-|F'+str(f-NUM_FEATURES)+' '
-			all_clauses[cur_clause].append(this_clause) #all_clauses[cur_clause][cur_cls] = this_clause
-			#print('CLASS :',cur_cls,' - CLAUSE ',cur_clause, ' : ', this_clause)
-		#print('\n\n')'''
-
-	for cur_clause in range(NUM_CLAUSES):
+	'''for cur_clause in range(NUM_CLAUSES):
 		for cur_cls in CLASSES:
 			for f in range(NUM_FEATURES*2):
 				print_str=str(ep)+'\t'+str(cur_cls) +'\t'+ str(cur_clause)+'\t'+ all_clauses[cur_clause][int(cur_cls)]
@@ -114,4 +98,4 @@ for ep in range(EPOCHS):
 
 				fb2_cnt=tm.get_typeII_clauses(int(cur_cls), cur_clause, f)
 				print_str+=str(fb2_cnt)
-				print(print_str)
+				print(print_str)'''
