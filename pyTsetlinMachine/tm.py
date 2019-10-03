d560c629f7dcc63e39ab5a43acc1ffeee5b90fd6
Rupsa Saha <rupsajina@gmail.com>
2019-10-03 14:44:43 +0200

    Update tm.py

--- pyTsetlinMachine/tm.py
+++ pyTsetlinMachine/tm.py
@@ -241,14 +241,13 @@ class MultiClassTsetlinMachine():
 	def ta_action(self, mc_tm_class, clause, ta):
 		return _lib.mc_tm_ta_action(self.mc_tm, mc_tm_class, clause, ta)
 	
-	def get_typeII_clauses(self,list_of_classes,num_of_clauses, num_of_features):
+	def get_all_clauses(self,list_of_classes,num_of_clauses, num_of_features):
 		all_clauses=[[] for i in range (num_of_clauses)]
-
 		for cur_clause in range(num_of_clauses):
 			for cur_cls in list_of_classes:
 				this_clause=''
 				for f in range(num_of_features*2):
-					action = tm.ta_action(int(cur_cls), cur_clause, f)
+					action = ta_action(int(cur_cls), cur_clause, f)
 					if action==1:
 						if this_clause!='':
 							this_clause+='AND '
@@ -259,8 +258,8 @@ class MultiClassTsetlinMachine():
 				all_clauses[cur_clause].append(this_clause) #all_clauses[cur_clause][cur_cls] = this_clause
 		return all_clauses
 		
-	#def get_typeII_clauses(self, mc_tm_class, clause, ta):
-	#	return _lib.mc_tm_typeII_clause(self.mc_tm, mc_tm_class, clause, ta)
+	def get_typeII_clauses(self, mc_tm_class, clause, ta):
+		return _lib.mc_tm_typeII_clause(self.mc_tm, mc_tm_class, clause, ta)
 
 	def get_state(self):
 		state_list = []
