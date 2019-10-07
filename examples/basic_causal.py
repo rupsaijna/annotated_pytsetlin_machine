bdfb1870ffd9f9ebc24e563e2033ef59c43e71be
Rupsa Saha <rupsajina@gmail.com>
2019-10-07 15:52:51 +0200

    Create basic_causal.py

---
+++ examples/basic_causal.py
@@ -0,0 +1,14 @@
+#identify if a sentence is causal or not (presence of causal connective)
+inp='is_causal_data.txt'
+
+sents=[]
+labels=[]
+
+for line in open(inp).readlines():
+  line=line.replace('\n','')line.split('\t')
+  words=line[0].split(' ')
+  sents.append(words)
+  labels.append(line[1])
+  
+print(sent)
+print(labels)
