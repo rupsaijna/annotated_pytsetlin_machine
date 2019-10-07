19dbf4b1bffc3a153ef2eae0059bf5d4cb21f4f6
Rupsa Saha <rupsajina@gmail.com>
2019-10-07 16:16:57 +0200

    Update basic_causal.py

--- examples/basic_causal.py
+++ examples/basic_causal.py
@@ -4,10 +4,12 @@ inp='is_causal_data.txt'
 
 sents=[]
 labels=[]
+all_words=[]
 
 for line in open(inp).readlines():
   line=line.replace('\n','').split('\t')
   words=line[0].split(' ')
+  all_words+=words
   sents.append(words)
   labels.append(int(line[1]))
   
