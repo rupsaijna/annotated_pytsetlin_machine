db0bfb1e28a6f42bd912cd80589ca83db802ccb4
Rupsa Saha <rupsajina@gmail.com>
2019-10-08 12:43:49 +0200

    Update basic_causal.py

--- examples/basic_causal.py
+++ examples/basic_causal.py
@@ -73,7 +73,7 @@ print('Num Features: ', NUM_FEATURES)
 for cur_clause in range(NUM_CLAUSES):
 	for cur_cls in CLASSES:
 		this_clause=''
-		for f in range(1,NUM_FEATURES*2+1):
+		for f in range(1,NUM_FEATURES*2):
 			print(f, NUM_FEATURES,f-NUM_FEATURES+1 )
 			action = tm.ta_action(int(cur_cls), cur_clause, f)
 			if action==1:
