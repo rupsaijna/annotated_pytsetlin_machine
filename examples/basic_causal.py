3770849ad4300fedae4157124ec4a44e8a5fa0f2
Rupsa Saha <rupsajina@gmail.com>
2019-10-09 09:35:20 +0200

    Update basic_causal.py

--- examples/basic_causal.py
+++ examples/basic_causal.py
@@ -45,10 +45,9 @@ reverse_word_map = dict(map(reversed, word_idx.items()))
 data=encode_sentences(sents)
 
 print(reverse_word_map)
-gfds
 #print(word_idx)
-#print(sents[10], data[10])
-
+print(sents[10], data[10])
+hkj
 x_train, x_test, y_train, y_test = train_test_split(data, labels)
 x_train_ids=x_train[:,-1]
 x_test_ids=x_test[:,-1]
