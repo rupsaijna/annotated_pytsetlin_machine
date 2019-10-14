1873ffc20323c6b73df4e6ba150c1196fc9e0b91
Rupsa Saha <rupsajina@gmail.com>
2019-10-14 10:20:28 +0200

    Update semeval_to_csv.py

--- examples/semeval/semeval_to_csv.py
+++ examples/semeval/semeval_to_csv.py
@@ -1,3 +1,8 @@
+import re
+
+regex = r"<\/*e\d{1}>"
+subst = ""
+
 inp_file='training_full.txt'
 out_file='training.csv'
 
@@ -10,12 +15,13 @@ for ln in open(inp_file,'r').readlines():
     if line_num%4==0:
         sent=ln.replace('\n','').split('\t')
         sent=sent[1].strip()
+        sent=re.sub(regex, subst, sent, 0, re.MULTILINE)
     if line_num%4==1:
         if 'Cause-Effect' in ln:
             label='1'
         else:
             label='0'
-        fo.write(sent+'\t'+label+'\n')
+        fo.write(sent[1:-1]+'\t'+label+'\n')
     line_num+=1
 fo.close()
     
