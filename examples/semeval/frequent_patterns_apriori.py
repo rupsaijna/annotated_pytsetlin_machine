c8efcefc0401948f0b4116c4b9efe05272c1f033
Rupsa Saha <rupsajina@gmail.com>
2019-10-25 10:37:07 +0200

    Create frequent_patterns_apriori.py

---
+++ examples/semeval/frequent_patterns_apriori.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+
+input_clause='clause_details20191023-074616.txt'
+data=pd.read_csv(input, sep='\t', na_filter = False)
+df_clause= data[['Clause']].copy()
+
+input_features='clause_details20191023-074616.txt'
+df_features=pd.read_csv(input, sep='\t', na_filter = False)
+df_clause['Extended']=[]
+
+for index, row in df_clause:
+    cl=row['Clause']
+    cl_list=cl.split(';')
+    ext_cl=[]
+    for c in cl_list:
+        if '#' not in c:
+            ext_cl.append(df_features.loc[df_features['fnum'] == int(c)])
+        else:
+             ext_cl.append('#'+df_features.loc[df_features['fnum'] == int(c.replace('#',''))])
+    df_clause.iloc[index]['Extended']=ext_cl
+    
+print(df_clause)
+jhfrgk
+
+te = TransactionEncoder()
+te_ary = te.fit(dataset).transform(dataset)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+frequent_itemsets=apriori(df, min_support=0.6, use_colnames=True)
+print(frequent_itemsets)
+
+fi=frequent_itemsets[0]
+print('Frequent Itemset',fi)
+
+
+
+'''frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
+frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+print(frequent_itemsets)'''
