c27ff69e9a479f78fff1754191c9340406a5b976
Rupsa Saha <rupsajina@gmail.com>
2019-10-25 10:13:24 +0200

    Update longest_repeating_subclauses.py

--- examples/semeval/longest_repeating_subclauses.py
+++ examples/semeval/longest_repeating_subclauses.py
@@ -6,9 +6,31 @@ data=pd.read_csv(input, sep='\t', na_filter = False)
 multiples={}
 full_clauses_count=0
 
-clauses=data['Clause']
+clauses=data['Clause'].values()
 
-print(clauses)
-        
-print(full_clauses_count)
-print(multiples)
+def extend_clauses(clauses):
+  for c in clauses:
+    c_list=c.split(';')
+    for i in range(0,num_clauses):
+      if str(i) not in c_list:
+        if i==0:
+          c_list.insert(0,'*'+str(i))
+        if i>0:
+          try:
+            prev_loc=c_list.index(str(i-1))
+          except:
+            prev_loc=c_list.index('*'+str(i-1))
+          c_list.insert(prev_loc+1,'*'+str(i))
+    for i in range(0,num_clauses):
+      if '#'+str(i) not in c_list:
+        if i==0:
+          c_list.insert(num_clauses+0,'*#'+str(i))
+        if i>0:
+          try:
+            prev_loc=c_list.index('#'+str(i-1))
+          except:
+            prev_loc=c_list.index('*#'+str(i-1))
+          c_list.insert(prev_loc+1,'*#'+str(i))
+    extended_clauses.append(';'.join(c_list))
+    return extended_clauses     
+    
