0b2b302f0586a7474e8fa53fe81a1a7707ff8a5c
Rupsa Saha <rupsajina@gmail.com>
2019-10-25 11:30:51 +0200

    Update frequent_patterns_apriori.py

--- examples/semeval/frequent_patterns_apriori.py
+++ examples/semeval/frequent_patterns_apriori.py
@@ -3,7 +3,10 @@ from mlxtend.preprocessing import TransactionEncoder
 
 input_clause='clause_details20191023-120835.txt'
 data=pd.read_csv(input_clause, sep='\t', na_filter = False)
+data=data.head()
 df_clause= data[['Clause']].copy()
+print(df_clause)
+dshjf
 
 input_features='feature_details20191023-120835.txt'
 df_features=pd.read_csv(input_features, sep='\t', na_filter = False)
