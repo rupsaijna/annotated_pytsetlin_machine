1aaa719cab3cb1f106b96bb2285b61b17d85596e
Rupsa Saha <rupsajina@gmail.com>
2019-10-25 11:08:04 +0200

    Update frequent_patterns_apriori.py

--- examples/semeval/frequent_patterns_apriori.py
+++ examples/semeval/frequent_patterns_apriori.py
@@ -9,6 +9,8 @@ input_features='clause_details20191023-074616.txt'
 df_features=pd.read_csv(input_features, sep='\t', na_filter = False)
 #df_clause['Extended']=[]
 
+print(df_features.columns)
+
 for index, row in df_clause.iterrows():
     cl=row['Clause']
     cl_list=cl.split(';')
