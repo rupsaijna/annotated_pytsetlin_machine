e98072e71ef8fc91130e7e4f4b30edf4bbac5ff9
Rupsa Saha <rupsajina@gmail.com>
2019-10-28 08:25:49 +0100

    Update frequent_patterns_apriori.py

--- examples/semeval/frequent_patterns_apriori.py
+++ examples/semeval/frequent_patterns_apriori.py
@@ -5,6 +5,8 @@ from mlxtend.frequent_patterns import apriori
 input_clause='clause_details20191023-120835.txt'
 data=pd.read_csv(input_clause, sep='\t', na_filter = False)
 
+data=data.head(50)
+
 dataset=[]
 for ind, row in data.iterrows():
     cl=row['Clause'].split(';')[:-1]
