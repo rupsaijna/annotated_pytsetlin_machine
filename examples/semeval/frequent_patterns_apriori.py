e4ac7494f188bd8dffe70182733264bb4e165e2d
Rupsa Saha <rupsajina@gmail.com>
2019-10-28 11:32:28 +0100

    Update frequent_patterns_apriori.py

--- examples/semeval/frequent_patterns_apriori.py
+++ examples/semeval/frequent_patterns_apriori.py
@@ -45,7 +45,7 @@ sparse_df = pd.SparseDataFrame(oht_ary, columns=te.columns_, default_fill_value=
 #print (sparse_df)
 '''
 
-frequent_itemsets=apriori(df, min_support=0.2, use_colnames=True)
+frequent_itemsets=apriori(df, min_support=0.09, use_colnames=True)
 
 frequent_itemsets = frequent_itemsets.sort_values(by='support', ascending=False)
 
