5665bb8676e52ce4b640b01b86e49bbf1086594d
Rupsa Saha <rupsajina@gmail.com>
2019-10-29 12:26:01 +0100

    Update semeval_to_csv.py

--- examples/semeval/data/semeval_to_csv.py
+++ examples/semeval/data/semeval_to_csv.py
@@ -4,7 +4,7 @@ regex = r"<\/*e\d{1}>"
 subst = ""
 
 inp_file='../training_full.txt'
-out_file='training_entity_destination.csv'
+out_file='training_instrument_agency.csv'
 
 fo=open(out_file,'w')
 
@@ -17,11 +17,10 @@ for ln in open(inp_file,'r').readlines():
         sent=sent[1].strip()
         sent=re.sub(regex, subst, sent, 0, re.MULTILINE)
     if line_num%4==1:
-        if 'Entity-Destination' in ln:
+        if 'Instrument-Agency' in ln:
             label='1'
         else:
             label='0'
         fo.write(sent[1:-1]+'\t'+label+'\n')
     line_num+=1
 fo.close()
-    
