7ba635b3a7d7b674d2a89a590538ae01254a18eb
Rupsa Saha <rupsajina@gmail.com>
2019-10-30 09:15:17 +0100

    Update svm.py

--- examples/semeval/baseline/svm.py
+++ examples/semeval/baseline/svm.py
@@ -1,5 +1,5 @@
-from sklearn.cross_validation import train_test_split
-#from sklearn.model_selection import train_test_split #py3
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split #py3
 from sklearn.feature_extraction import stop_words
 import numpy as np
 from nltk.util import ngrams,everygrams
