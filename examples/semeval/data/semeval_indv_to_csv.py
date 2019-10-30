84f06c96c785290871b7a438e21329f1c6884431
Rupsa Saha <rupsajina@gmail.com>
2019-10-30 13:58:11 +0100

    Update semeval_indv_to_csv.py

--- examples/semeval/data/semeval_indv_to_csv.py
+++ examples/semeval/data/semeval_indv_to_csv.py
@@ -4,7 +4,7 @@ regex = r"<\/*e\d{1}>"
 subst = ""
 
 inp_file='../training_full.txt'
-out_file='training_member_collection.csv'
+out_file='training_message_topic.csv'
 
 fo=open(out_file,'w')
 
@@ -17,7 +17,7 @@ for ln in open(inp_file,'r').readlines():
         sent=sent[1].strip()
         sent=re.sub(regex, subst, sent, 0, re.MULTILINE)
     if line_num%4==1:
-        if 'Member-Collection' in ln:
+        if 'Message-Topic' in ln:
             label='1'
         else:
             label='0'
