294c4db109c6b152d26a59d4d7ea9ecc047ed65c
Rupsa Saha <rupsajina@gmail.com>
2019-10-31 09:54:28 +0100

    Update frequent_patterns_apriori.py

--- examples/semeval/frequent_patterns_apriori.py
+++ examples/semeval/frequent_patterns_apriori.py
@@ -2,9 +2,10 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
 
-file_date='20191023-120835'
+fp='data/entdest/entdest_'
+file_date='20191028-123548'
 
-input_clause='clause_details'+file_date+'.txt'
+input_clause=fp+'clause_details'+file_date+'.txt'
 data=pd.read_csv(input_clause, sep='\t', na_filter = False)
 
 #data=data.head(500)
@@ -14,26 +15,6 @@ for ind, row in data.iterrows():
     cl=row['Clause'].split(';')[:-1]
     cl=[c.strip() for c in cl]
     dataset.append(cl)
-
-
-'''
-df_clause= data[['Clause']].copy()
-df_clause['Extended']=''
-
-##replacing feature_numbers with features
-for index, row in df_clause.iterrows():
-    cl=row['Clause']
-    cl_list=cl.split(';')[:-1]
-    ext_cl=[]
-    for c in cl_list:
-        if '#' not in c:
-            ext_cl.append(str(df_features.loc[df_features['fnum'] == int(c),'feature'].item()))
-        else:
-             ext_cl.append('#'+str(df_features.loc[df_features['fnum'] == int(c.replace('#','')),'feature'].item()))
-    df_clause.iloc[index]['Extended']=ext_cl
-
-##Working with clauses with feature names
-dataset=df_clause[['Extended']].values'''
 
 ##one hot encoding, sparse
 te = TransactionEncoder()
@@ -53,7 +34,7 @@ frequent_itemsets = frequent_itemsets.sort_values(by='support', ascending=False)
 
 frequent_itemsets['Word_clause']=''
 
-input_features='feature_details'+file_date+'.txt'
+input_features=fp+'feature_details'+file_date+'.txt'
 df_features=pd.read_csv(input_features, sep='\t', na_filter = False)
 
 for idx, row in frequent_itemsets.iterrows():
@@ -68,16 +49,18 @@ for idx, row in frequent_itemsets.iterrows():
         
 #print(frequent_itemsets)
 
-with open('meta_details'+file_date+'.txt') as f:
+with open(fp+'meta_details'+file_date+'.txt') as f:
     lines = f.readlines()
     with open('frequent_itemsets_details'+file_date+'.csv', 'w') as f1:
         f1.writelines(lines)
-f1= open('frequent_itemsets_details'+file_date+'.csv', 'a+')
+f1= open(fp+'frequent_itemsets_details'+file_date+'.csv', 'a+')
 f1.write('\n\n')
 f1.close()
           
-with open('frequent_itemsets_details'+file_date+'.csv', 'a') as f:
+with open(fp+'frequent_itemsets_details'+file_date+'.csv', 'a') as f:
     frequent_itemsets.to_csv(f, sep='\t')
+    
+    
 '''
 ##adding length filter
 frequent_itemsets = apriori(df, min_support=0.8, use_colnames=True)
