9364fb814ac134b49dda439b124c9fdd09b861e1
Rupsa Saha <rupsajina@gmail.com>
2019-11-04 15:56:12 +0100

    Update preprocess.py

--- examples/sentiment140/preprocess.py
+++ examples/sentiment140/preprocess.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import re
+regex=r"(?:\@|https?\://)\S+"
+text = re.sub(regex, "", text)
 
 df=pd.read_csv('training.1600000.processed.noemoticon.csv', header=None, names=['polarity', 'id','date','query','user','tweet'], encoding='latin-1')
 
@@ -8,4 +11,10 @@ fout='tweets_neutralnegative.csv'
 print(df)
 
 data=df.loc[df['polarity'].isin(allow_polarity)]
+
+for ind, row in data.iterrows():
+    text = re.sub(regex, "", row['tweet'])
+    data[ind, 'tweet']=text
+  
+
 data.to_csv(fout, columns=['id','tweet','polarity'], index=False, sep='\t', quoting=2)
