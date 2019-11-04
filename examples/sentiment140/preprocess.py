5b1b9cb8aab4fe1a47ce852cc834520245ea26f6
Rupsa Saha <rupsajina@gmail.com>
2019-11-04 15:21:58 +0100

    Create preprocess.py

---
+++ examples/sentiment140/preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df=pd.read_csv('training.1600000.processed.noemoticon.csv', cols=['polarity', 'id','date','query','user','tweet'])
+
+allow_polarity=['0','2'] #change
+fout='tweets_neutralnegative.csv'
+
+print(df)
+
+data=df.loc[df['polarity']isin(allow_polarity)]
+
+data=data['id','tweet','polarity'].copy()
+
+print(data)
+data.to_csv(fout, index=False)
