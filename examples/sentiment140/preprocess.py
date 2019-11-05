d61a8ee8b10644f26b0ed755f62b16f499da8297
Rupsa Saha <rupsajina@gmail.com>
2019-11-05 13:50:00 +0100

    Update preprocess.py

--- examples/sentiment140/preprocess.py
+++ examples/sentiment140/preprocess.py
@@ -7,27 +7,22 @@ regex=r"(?:\@|https?\://)\S+"
 
 df=pd.read_csv('training.1600000.processed.noemoticon.csv', header=None, names=['polarity', 'id','date','query','user','tweet'], encoding='latin-1')
 
-allow_polarity=['0','2'] #change
-fout='tweets_neutralnegative.csv'
+allow_polarity=['0','4'] #change
+fout='tweets_positivenegative.csv'
 
 data=df.loc[df['polarity'].isin(allow_polarity)]
 
 for ind, row in tqdm(data.iterrows(), total=data.shape[0]):
-    '''text = re.sub(regex, "", row['tweet'])
+    text = re.sub(regex, "", row['tweet'])
     text=text.encode('ascii', 'ignore').decode('ascii')
     text=text.replace('...',' ELLIPSIS ')
     text=text.replace('..',' ELLIPSIS ')
     text=text.replace('  ',' ')
     text=text.strip()
     #text=text.translate(str.maketrans('','',string.punctuation))
-    data.loc[ind, 'tweet']=text'''
-    if row['polarity']==2:
-        print (row)
-        break
-    if row['polarity']==0:
+    data.loc[ind, 'tweet']=text
+    if row['polarity']==4:
         data.loc[ind, 'polarity']==1
-    else:
-        data.loc[ind, 'polarity']==0
   
 
 data.to_csv(fout, columns=['id','tweet','polarity'], index=False, sep='\t', quoting=2)
