beb7f630e0982bf50af93774893abbb473994c55
Rupsa Saha <rupsajina@gmail.com>
2019-11-05 12:16:21 +0100

    Update preprocess.py

--- examples/sentiment140/preprocess.py
+++ examples/sentiment140/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import string
+import emot
 regex=r"(?:\@|https?\://)\S+"
 
 df=pd.read_csv('training.1600000.processed.noemoticon.csv', header=None, names=['polarity', 'id','date','query','user','tweet'], encoding='latin-1')
@@ -14,7 +15,10 @@ data=df.loc[df['polarity'].isin(allow_polarity)]
 
 for ind, row in data.iterrows():
     text = re.sub(regex, "", row['tweet'])
-    text=text.translate(str.maketrans('','',string.punctuation))
+    text=text.encode('ascii', 'ignore').decode('ascii')
+    emojis=emot.emoji(text)
+    print(emojis)
+    #text=text.translate(str.maketrans('','',string.punctuation))
     data.loc[ind, 'tweet']=text
     break
   
