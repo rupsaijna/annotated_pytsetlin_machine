1da2edd5bdbea3d6e6abe5e3506a33f7e51499f0
Rupsa Saha <rupsajina@gmail.com>
2019-11-05 12:01:48 +0100

    Update preprocess.py

--- examples/sentiment140/preprocess.py
+++ examples/sentiment140/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import string
 regex=r"(?:\@|https?\://)\S+"
 
 df=pd.read_csv('training.1600000.processed.noemoticon.csv', header=None, names=['polarity', 'id','date','query','user','tweet'], encoding='latin-1')
@@ -13,8 +14,8 @@ data=df.loc[df['polarity'].isin(allow_polarity)]
 
 for ind, row in data.iterrows():
     text = re.sub(regex, "", row['tweet'])
+    text=text.translate(str.maketrans('','',string.punctuation))
     data.loc[ind, 'tweet']=text
-    print(data.loc[ind])
     break
   
 
