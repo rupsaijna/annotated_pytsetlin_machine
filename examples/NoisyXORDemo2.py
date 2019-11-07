ce0776126138e29f722b696823618d3e621f0999
Rupsa Saha <rupsajina@gmail.com>
2019-11-07 15:45:05 +0100

    Update NoisyXORDemo2.py

--- examples/NoisyXORDemo2.py
+++ examples/NoisyXORDemo2.py
@@ -21,16 +21,16 @@ test_data = np.loadtxt("NoisyXORTestData.txt")
 X_test = test_data[:,0:-1] #last column is class labels
 Y_test = test_data[:,-1]  #last column is class labels
 
-#Initialize the Tsetlin Machine
+'''#Initialize the Tsetlin Machine
 tm = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
 
 #Fit TM on training data
 tm.fit(X_train, Y_train, epochs=1)
 
 ##save
-tm.save_model('tm_model.npz')
+tm.save_model('tm_model.npz')'''
 
-'''print ('X_train.shape ',X_train.shape)
+print ('X_train.shape ',X_train.shape)
 print ('Y_train.shape ',Y_train.shape)
 
 newX=np.ones(X_train.shape)
@@ -40,7 +40,7 @@ print ('Y.shape ',newY.shape)
 
 tm2 = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
 tm2.fit(newX, newY, epochs=0)
-ta_state_loaded = np.load("tm_full66.npz")['arr_0']
+ta_state_loaded = np.load("tm_model.npz")['arr_0']
 tm2.set_state(ta_state_loaded)
 
 
