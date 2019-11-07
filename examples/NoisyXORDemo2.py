1d85a9f13a8f27474bd06cd18e35162e513bf282
Rupsa Saha <rupsajina@gmail.com>
2019-11-07 16:08:18 +0100

    Update NoisyXORDemo2.py

--- examples/NoisyXORDemo2.py
+++ examples/NoisyXORDemo2.py
@@ -21,14 +21,14 @@ test_data = np.loadtxt("NoisyXORTestData.txt")
 X_test = test_data[:,0:-1] #last column is class labels
 Y_test = test_data[:,-1]  #last column is class labels
 
-'''#Initialize the Tsetlin Machine
+#Initialize the Tsetlin Machine
 tm = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
 
 #Fit TM on training data
 tm.fit(X_train, Y_train, epochs=1)
 
 ##save
-tm.save_model('tm_model.npz')'''
+tm.save_model('tm_model.npz')
 
 print ('X_train.shape ',X_train.shape)
 print ('Y_train.shape ',Y_train.shape)
@@ -41,10 +41,8 @@ print ('Y.shape ',newY.shape)
 #MultiClassTsetlinMachine.load_model()
 tm2 = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
 tm2.fit(newX, newY, epochs=0)
-ta_state_loaded = np.load("tm_model.npz")['arr_0']
+ta_state_loaded = np.load("tm_model.npz")['states']
 tm2.set_state(ta_state_loaded)
-
-
 
 #Predict on test data, compare to ground truth, calculate accuracy0
 #print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
