b42c5b7430994e6c41b2b75ad864e6596a30c6db
Rupsa Saha <rupsajina@gmail.com>
2019-11-08 10:53:15 +0100

    Update NoisyXORDemo2.py

--- examples/NoisyXORDemo2.py
+++ examples/NoisyXORDemo2.py
@@ -28,11 +28,11 @@ tm = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_fee
 tm.fit(X_train, Y_train, epochs=1)
 
 ##save
-#tm.save_model('tm_model.npz')
+tm.save_model('tm_model.npz')
 
 tm2=MultiClassTsetlinMachine.load_model('tm_model.npz')
 
 
 #Predict on test data, compare to ground truth, calculate accuracy0
-#print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
+print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
 print("Accuracy after saving:", 100*(tm2.predict(X_test) == Y_test).mean())
