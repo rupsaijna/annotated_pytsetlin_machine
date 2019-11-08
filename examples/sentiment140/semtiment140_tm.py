976dbe769a76201f78c41a2b9bb7f78c1b41a2b2
Rupsa Saha <rupsajina@gmail.com>
2019-11-08 12:58:29 +0100

    Create semtiment140_tm.py

---
+++ examples/sentiment140/semtiment140_tm.py
@@ -0,0 +1,41 @@
+#identify if a sentence is Positive or Negative
+
+import sys
+sys.path.append('../../../pyTsetlinMachine/')
+from tm import MultiClassTsetlinMachine
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import stop_words
+import numpy as np
+from nltk.util import ngrams,everygrams
+import re
+import string
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+clause_file='senti140_clause_details'+timestr+'.txt'
+feature_file='senti140_feature_details'+timestr+'.txt'
+meta_file='senti140_meta_details'+timestr+'.txt'
+
+RUNS=1
+
+inp='tweets_positivenegative.csv'
+
+sents=[]
+labels=[]
+all_words=[]
+
+def encode_sentences(txt):
+	feature_set=np.zeros((len(txt), len(word_set)+1),dtype=int)
+	tnum=0
+	for t in txt:
+		s_words=t[1:]+list(set(list(everygrams(t[1:], min_len=2,max_len=2))))
+		for w in s_words:
+			idx=word_idx[w]
+			feature_set[tnum][idx]=1
+		feature_set[tnum][-1]=t[0]
+		tnum+=1
+	return feature_set
+
+df=pd.read_csv(inp)
+data=df.iloc[np.r_[0:2, -2:0]]
+
+print(data)
