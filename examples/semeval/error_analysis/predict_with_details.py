47492e6bc5043365298d7fa9a339e380248f020d
Rupsa Saha <rupsajina@gmail.com>
2019-11-10 17:20:25 +0100

    Update predict_with_details.py

--- examples/semeval/error_analysis/predict_with_details.py
+++ examples/semeval/error_analysis/predict_with_details.py
@@ -133,15 +133,6 @@ for i in range(len(x_test_ids)):
 				feature_vector[f+NUM_FEATURES]=action_negated
 				feature_count_plain[f]+=action_plain
 				feature_count_negated[f]+=action_negated
-				if action_plain==0 and action_negated==0:
-					feature_count_ignore += 1
-				feature_count_contradiction += action_plain and action_negated
-				if (cur_cls % 2 == 0):
-					feature_count_plain_positive[f] += action_plain
-    					feature_count_negated_positive[f] += action_negated
-   				else:
-					feature_count_plain_negative[f] += action_plain
-					feature_count_negated_negative[f] += action_negated
 				
 				if action_plain==1:
 					this_clause+=str(f)+';'
