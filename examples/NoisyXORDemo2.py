c4fa0cda40b69865b71f0f76e65d64f5e7e419c2
Rupsa Saha <rupsajina@gmail.com>
2019-11-19 11:30:20 +0100

    Update NoisyXORDemo2.py

--- examples/NoisyXORDemo2.py
+++ examples/NoisyXORDemo2.py
@@ -22,17 +22,17 @@ X_test = test_data[:,0:-1] #last column is class labels
 Y_test = test_data[:,-1]  #last column is class labels
 
 #Initialize the Tsetlin Machine
-tm = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
+#tm = MultiClassTsetlinMachine(NUM_CLAUSES, THRESHOLD, S, boost_true_positive_feedback=0)
 
 #Fit TM on training data
-tm.fit(X_train, Y_train, epochs=1)
+#tm.fit(X_train, Y_train, epochs=1)
 
 ##save
-tm.save_model('tm_model.npz')
+#tm.save_model('tm_model.npz')
 
-#tm2=MultiClassTsetlinMachine.load_model('tm_model.npz')
+tm2=MultiClassTsetlinMachine.load_model('tm_model.npz')
 
 
 #Predict on test data, compare to ground truth, calculate accuracy0
-print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
-#print("Accuracy after saving:", 100*(tm2.predict(X_test) == Y_test).mean())
+#print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
+print("Accuracy after saving:", 100*(tm2.predict(X_test) == Y_test).mean())
