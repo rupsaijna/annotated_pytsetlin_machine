c5468e8d462d4154ded0d9e7d5d51c3c3bf5ee72
Rupsa Saha <rupsajina@gmail.com>
2019-12-02 14:29:54 +0100

    Update lexicon_compare.py

--- examples/sentiment140/lexicon_compare.py
+++ examples/sentiment140/lexicon_compare.py
@@ -1,16 +1,21 @@
 import pandas as pd
 #import types
-
 #NumberTypes = (types.IntType, types.LongType, types.FloatType, types.ComplexType,np.float32)
+fp='../sentiment140/senti140_'
+file_date='20191108-133244'
+input_features=fp+'feature_details'+file_date+'.txt'
+df_features=pd.read_csv(input_features, sep='\t', na_filter = False)
+input_clauses=fp+'clause_details'+file_date+'.txt'
+df_clauses=pd.read_csv(input_clauses, sep='\t', na_filter = False).head(1)
+df_clause_positive=df_clauses.loc[df_clauses['p/n'] == 'positive'].copy()
+df_clause_negative=df_clauses.loc[df_clauses['p/n'] == 'negative'].copy()
+
 
 #generic
 word_dict={'negative':100,'positive':100,'anger':100,'sadness':100,'happiness':100,'fear':100, 'anticipation':100, 'trust':100, 'surprise':100, 'sadness':100, 'joy':100,'disgust':100}
 
 #outputfilename="lexiconscompared.txt"
 #fout = open(outputfilename,'w')
-
-featurefilename=""
-
 
 ########lex##############################
 print ("Loading lexicons...")
@@ -35,8 +40,73 @@ for ind,row in lex_files.iterrows():
 		else:
 			dict_df[row['file']]=pd.read_csv(row['file'],sep='\t',header=None).fillna('')
 	dict_counts[row['file']]['in_text']=[]
-	dict_counts[row['file']]['in_features']=[]
+	dict_counts[row['file']]['in_positive_features']=[]
+	dict_counts[row['file']]['in_negative_features']=[]
       
 ########lex##############################
 
-print(dict_df)
+covered=[]
+for idx, row in df_clause_positive.iterrows():
+	cl=row['Clause'].split(';')[:-1]
+    cl=[c.strip() for c in cl]
+	for this_feature in cl:
+		this_feature=this_feature.replace('#','')
+		if this_feature not in covered:
+			word_feature=str(df_features.loc[df_features['fnum'] == int(this_feature),'feature'].item())
+			try:
+				b=eval(word_feature)
+				if type(b) is tuple:
+					word_feature=' '.join(b)
+			except
+				word_feature=word_feature
+			covered.append(word_feature)
+			for l in dict_df:
+				df=dict_df[l]
+				det=lex_files[lex_files['file']==l]
+				words_in_det=list(det['word'].values)
+				if word_feature in words_in_det:
+					dict_counts[l]['in_positive_features'].append(word_feature)
+
+covered=[]
+for idx, row in df_clause_negative.iterrows():
+	cl=row['Clause'].split(';')[:-1]
+    cl=[c.strip() for c in cl]
+	for this_feature in cl:
+		this_feature=this_feature.replace('#','')
+		if this_feature not in covered:
+			word_feature=str(df_features.loc[df_features['fnum'] == int(this_feature),'feature'].item())
+			try:
+				b=eval(word_feature)
+				if type(b) is tuple:
+					word_feature=' '.join(b)
+			except
+				word_feature=word_feature
+			covered.append(word_feature)
+			for l in dict_df:
+				df=dict_df[l]
+				det=lex_files[lex_files['file']==l]
+				words_in_det=list(det['word'].values)
+				if word_feature in words_in_det:
+'''					dict_counts[l]['in_negative_features'].append(word_feature)
+covered=[]
+for idx, row in df_features.iterrows():
+	cl=row['feature']
+	if this_feature!='':
+		this_feature=this_feature.replace('#','')
+		if this_feature not in covered:
+			word_feature=str(df_features.loc[df_features['fnum'] == int(this_feature),'feature'].item())
+			try:
+				b=eval(word_feature)
+				if type(b) is tuple:
+					word_feature=' '.join(b)
+			except
+				word_feature=word_feature
+			covered.append(word_feature)
+			for l in dict_df:
+				df=dict_df[l]
+				det=lex_files[lex_files['file']==l]
+				words_in_det=list(det['word'].values)
+				if word_feature in words_in_det:
+					dict_counts[l]['in_text'].append(word_feature)
+'''
+print(dict_counts)
