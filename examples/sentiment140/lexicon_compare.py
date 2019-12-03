fdb0194d023a3362aa5ccd6fd07648998b2329ff
Rupsa Saha <rupsajina@gmail.com>
2019-12-03 10:09:41 +0100

    Update lexicon_compare.py

--- examples/sentiment140/lexicon_compare.py
+++ examples/sentiment140/lexicon_compare.py
@@ -75,26 +75,22 @@ for idx, row in df_clause_positive.iterrows():
 					dict_counts[l]['in_positive_features'].append(word_feature)
 					print('added')
 					added=1
-			print('Was added?', added)
 			if added==0 and ' ' in word_feature:
 				wf=word_feature.split(' ')
 				for eachword in wf:
-					print('Now each:', eachword)
 					for l in dict_df:
-						print('file:',l)
 						dff=dict_df[l]
 						det=lex_files[lex_files['file']==l]
 						word_location=det['word'].values[0]
 						words_in_det=list(dff.iloc[:,word_location].values)
 						if eachword in words_in_det:
 							dict_counts[l]['in_partial_positive_features'].append(eachword)
-							print('added')
 
 covered=[]
 for idx, row in df_clause_negative.iterrows():
 	cl=row['Clause'].split(';')[:-1]
 	cl=[c.strip() for c in cl]
-	print('\nNeg Clause:',cl)
+	print('\nPos Clause:',cl)
 	for this_feature in cl:
 		this_feature=this_feature.replace('#','')
 		if this_feature not in covered:
@@ -110,9 +106,10 @@ for idx, row in df_clause_negative.iterrows():
 			print('Feature:',word_feature)
 			added=0
 			for l in dict_df:
-				df=dict_df[l]
+				dff=dict_df[l]
 				det=lex_files[lex_files['file']==l]
-				words_in_det=list(det['word'].values)
+				word_location=det['word'].values[0]
+				words_in_det=list(dff.iloc[:,word_location].values)
 				if word_feature in words_in_det:
 					dict_counts[l]['in_negative_features'].append(word_feature)
 					added=1
@@ -120,9 +117,10 @@ for idx, row in df_clause_negative.iterrows():
 				wf=word_feature.split(' ')
 				for eachword in wf:
 					for l in dict_df:
-						df=dict_df[l]
+						dff=dict_df[l]
 						det=lex_files[lex_files['file']==l]
-						words_in_det=list(det['word'].values)
+						word_location=det['word'].values[0]
+						words_in_det=list(dff.iloc[:,word_location].values)
 						if eachword in words_in_det:
 							dict_counts[l]['in_partial_negative_features'].append(eachword)
 '''covered=[]
@@ -146,4 +144,5 @@ for idx, row in df_features.iterrows():
 				if word_feature in words_in_det:
 					dict_counts[l]['in_text'].append(word_feature)
 '''
-print(dict_counts)
+for d in dict_counts:
+	print(dict_counts[d])
