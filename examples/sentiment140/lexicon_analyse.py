e6c1a25ede8935178dacaa0ce440543498c7c567
Rupsa Saha <rupsajina@gmail.com>
2019-12-04 10:54:07 +0100

    Create lexicon_analyse.py

---
+++ examples/sentiment140/lexicon_analyse.py
@@ -0,0 +1,11 @@
+import pickle
+
+f = open("dict_cnts.pkl","rb")
+newdict=pickle.load(f)
+cntdict={}
+
+
+for d in newdict:
+	cntdict[d]={}
+	cntdict[d]['in_text_cnt']=len(set(newdict[d]['in_text']))
+	print('\n',d,newdict[d])
