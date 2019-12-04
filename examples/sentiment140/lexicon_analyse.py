c5f998a39b632c2cf8616645a025bf93752cd36d
Rupsa Saha <rupsajina@gmail.com>
2019-12-04 11:36:04 +0100

    Update lexicon_analyse.py

--- examples/sentiment140/lexicon_analyse.py
+++ examples/sentiment140/lexicon_analyse.py
@@ -8,6 +8,10 @@ cntdict={}
 for d in newdict:
 	cntdict[d]={}
 	cntdict[d]['in_text_cnt']=len(set(newdict[d]['in_text']))
+	cntdict[d]['in_positive_features_cnt']=len(set(newdict[d]['in_positive_features']))
+	cntdict[d]['in_negative_features_cnt']=len(set(newdict[d]['in_negative_features']))
+	
+	
 	#print('\n',d,newdict[d])
 
 	
