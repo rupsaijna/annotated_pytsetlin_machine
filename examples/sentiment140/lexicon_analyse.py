02577e0dee578a800b91cbe58e8da5bc48c70fff
Rupsa Saha <rupsajina@gmail.com>
2019-12-04 13:30:02 +0100

    Update lexicon_analyse.py

--- examples/sentiment140/lexicon_analyse.py
+++ examples/sentiment140/lexicon_analyse.py
@@ -11,7 +11,7 @@ for d in newdict:
 	cntdict[d]['in_positive_features_cnt']=len(set(newdict[d]['in_positive_features']))
 	cntdict[d]['in_negative_features_cnt']=len(set(newdict[d]['in_negative_features']))
 	cntdict[d]['pos_neg_features_cnt']=len(set(newdict[d]['in_negative_features']+newdict[d]['in_positive_features']))
-	cntdict[d]['pos_neg_intersection_cnt']=len(set(newdict[d]['in_negative_features'].intersection(set(newdict[d]['in_positive_features'])))
+	cntdict[d]['pos_neg_intersection_cnt']=len(set(newdict[d]['in_negative_features']).intersection(set(newdict[d]['in_positive_features'])))
 	cntdict[d]['text_positive_features_cnt']=len(set(newdict[d]['in_text']).intersection(set(newdict[d]['in_positive_features'])))
 	cntdict[d]['text_negative_features_cnt']=len(set(newdict[d]['in_text']).intersection(set(newdict[d]['in_negative_features'])))
 	
