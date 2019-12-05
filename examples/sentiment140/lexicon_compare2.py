8fd314d3663afbb3e1d39ada4b24d90719d7d3f9
Rupsa Saha <rupsajina@gmail.com>
2019-12-05 16:08:58 +0100

    Update lexicon_compare2.py

--- examples/sentiment140/lexicon_compare2.py
+++ examples/sentiment140/lexicon_compare2.py
@@ -136,10 +136,34 @@ for idx, row in df_features.iterrows():
 				dict_counts[l]['in_text'].append(word_feature)
 
 				
-for d in dict_counts:
+'''for d in dict_counts:
 	print(d, dict_counts[d])
+'''	
+	
 				
-'''f = open("dict_cnts2.pkl","wb")
+f = open("dict_cnts2.pkl","wb")
 pickle.dump(dict_counts,f)
 f.close()
-'''
+
+
+f = open("dict_cnts2.pkl","rb")
+newdict=pickle.load(f)
+cntdict={}
+
+
+for d in newdict:
+	cntdict[d]={}
+	cntdict[d]['in_text_cnt']=len(set(newdict[d]['in_text']))
+	cntdict[d]['in_positive_features_cnt']=len(set(newdict[d]['in_positive_features']))
+	cntdict[d]['in_negative_features_cnt']=len(set(newdict[d]['in_negative_features']))
+	cntdict[d]['pos_neg_features_cnt']=len(set(newdict[d]['in_negative_features']+newdict[d]['in_positive_features']))
+	cntdict[d]['pos_neg_intersection_cnt']=len(set(newdict[d]['in_negative_features']).intersection(set(newdict[d]['in_positive_features'])))
+	cntdict[d]['text_positive_features_cnt']=len(set(newdict[d]['in_text']).intersection(set(newdict[d]['in_positive_features'])))
+	cntdict[d]['text_negative_features_cnt']=len(set(newdict[d]['in_text']).intersection(set(newdict[d]['in_negative_features'])))
+	
+	
+	#print('\n',d,newdict[d])
+
+
+for d in cntdict:
+	print('\n',d,cntdict[d])
